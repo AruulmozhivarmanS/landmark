b4a2531334a1912067489b79599bad80966bc07a
Aruul Mozhi Varman S <aruulmozhivarman@outlook.com>
2021-07-01 15:27:13 +0530

    Chenged to new strorage scheme that includes tags

--- sync_npy.py
+++ sync_npy.py
@@ -8,6 +8,7 @@ import numpy as np
 import rosbag
 import rospy
 import os
+import pickle
 
 save_dir = '/home/anushl9o5/Desktop/RBCCPS/Garrett/landmark_v2/left_side_data_man/13_npy'
 saved = False
@@ -21,76 +22,76 @@ for i in [1, 8, 12, 13]:
 exit()
 '''
 
-origin_tag = '0'
-all_tags = ['0', '1', '2', '3', '4', '5', '16', '7', '8', '9', '10', '11', '17', '14', '15']
+# origin_tag = '0'
+# all_tags = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13',]
 
 rospy.init_node("data_node", anonymous=True)
 
 buf = tf.Buffer()
 ls = tf.TransformListener(buf)
 
-gt_dict = dict()
-data_dict = dict()
+gt_global = []
+data_global = []
+tag_global = []
 
+gt_np = []
+data_np = []
 
 def data_synced(tag, tag_odom):
-    global origin_tag, data_dict, gt_dict
+    # global origin_tag
 
-    if len(tag.detections) > 0:
-        if len(tag.detections) > 1:
-            detected_tag = sort_l2(tag.detections)
-        else:
-            detected_tag = tag.detections[0]
+    global tag_global, data_global, gt_global
+    gt_array = []
+    tag_array = []
+    data_array = []
 
-        tag_id = str(detected_tag.id[0])
+    for tag in tag.detections:
+        tag_id = str(tag)
         try:
-            data_dict[tag_id].append(buf.lookup_transform('tag_'+tag_id, 'base_link', rospy.Time(0), rospy.Duration(0.01)))
-            gt_dict[tag_id].append(tag_odom)
+            data_array.append(buf.lookup_transform('tag_'+tag_id, 'base_link', rospy.Time(0), rospy.Duration(0.01)))
+            gt_array.append(tag_odom)
+            tag_array.append(tag)
             print(tag_id)
-
         except:
             pass
 
+    tag_global.append(tag_array)
+    gt_global.append(gt_array)
+    data_global.append(data_array)
+
 def convert_npy():
-    global gt_dict, data_dict, save_dir
+    global gt_global, tag_global, data_global
+    global gt_np, data_np
 
-    for key in gt_dict:
-        gt_list = []
-        for gt_msgs in gt_dict[key]:
-            gt_list.append([gt_msgs.pose.pose.position.x, gt_msgs.pose.pose.position.y, gt_msgs.pose.pose.position.z, gt_msgs.pose.pose.orientation.x, gt_msgs.pose.pose.orientation.y, gt_msgs.pose.pose.orientation.z, gt_msgs.pose.pose.orientation.w])
-        
-        gt_list = np.asarray(gt_list)
-        if len(gt_list) > 0:
-            print(gt_list.shape)
-            np.save(os.path.join(save_dir, 'gt', key+'.npy'), gt_list)
+    for gts, odoms in zip(gt_global, data_global):
+        gt_np.append(convert_msg_array(gts))
+        data_global.append(convert_msg_array(odoms))
+    
+    with open("gt_1.txt", "wb") as fp:
+        pickle.dump(gt_np, fp)
+    
+    with open("tag_1.txt", "wb") as fp:
+        pickle.dump(tag_global, fp)
+    
+    with open("data_1.txt", "wb") as fp:
+        pickle.dump(data_np, fp)
+    
 
-    for key in data_dict:
-        data_list = []
-        for data_msgs in data_dict[key]:
-            data_list.append([data_msgs.transform.translation.x, data_msgs.transform.translation.y, data_msgs.transform.translation.z, data_msgs.transform.rotation.x, data_msgs.transform.rotation.y, data_msgs.transform.rotation.z, data_msgs.transform.rotation.w])
-        
-        data_list = np.asarray(data_list)
-        if len(data_list) > 0:
-            print(data_list.shape)
-            np.save(os.path.join(save_dir, 'data', key+'.npy'), data_list)
 
+def convert_msg_array(msgs):
+    msg_array = []
+    for msg in msgs:
+        msg_array.append([msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z, msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w])
+
+    return msg_array
 
 def save_data(msg):
-    global gt_dict, data_dict, saved
+    global saved
     if not saved:
-        for key in gt_dict:
-            print(len(gt_dict[key]), len(data_dict[key]))
         convert_npy()
         saved = True
 
-
-
 if __name__ == "__main__":
-
-    for tag_id in all_tags:
-        #if tag_id == origin_tag:
-        data_dict[tag_id] = []
-        gt_dict[tag_id] = []
 
     bag_done = rospy.Subscriber('/done', String, save_data)
     odom_sub = message_filters.Subscriber('/odometry/filtered', Odometry)
